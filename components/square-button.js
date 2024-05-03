5a5afdec77f082d445d77e3a7510e0c660f03d73
tingfei <1228959039@qq.com>
2024-05-03 23:56:30 +0800

    feat: add square button

---
+++ components/square-button.js
@@ -0,0 +1,44 @@
+const template = document.createElement("template");
+
+template.innerHTML = `
+    <style>
+    button {
+        background-color: white;
+        color: white;
+        border: none;
+        padding: 10px;
+        margin: 10px;
+        border-radius: 5px;
+        cursor: pointer;
+      }
+      button:hover {
+        box-shadow: 2px 2px 2px black;
+      }
+      button:active {
+        box-shadow: 1px 1px 1px black;
+      }
+    </style>
+    <button id="squareButton">
+      <div style="padding: 10px; border: 1px solid black;" >
+      </div>
+    </button>
+`;
+
+class SquareButton extends HTMLElement {
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.appendChild(template.content.cloneNode(true));
+
+    const button = shadowRoot.getElementById("squareButton");
+
+    button.addEventListener("click", () => {
+      console.log("world");
+      this.dispatchEvent(
+        new CustomEvent("button-click", { detail: "Button was clicked!" })
+      );
+    });
+  }
+}
+
+customElements.define("square-button", SquareButton);
